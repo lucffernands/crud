26ca52826329f7fb348b3874c97c186cfb5e2072
Luciano Fernandes <lucianoffernands@gmail.com>
2023-01-09 15:05:02 -0300

    Configured the connection H2 and seeding database

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('José Silva', '111.111.111-11', 3500.0, '2001-07-25', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Tiago Faria', '222.222.222-22', 3200.0, '2000-03-16', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Maria do Rosário', '333.333.333-33', 4000.0, '1998-03-10', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Teresa Silva', '444.444.444-44', 4500.0, '1999-10-10', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Maria Brown', '555.555.555-55', 3000.0, '2001-07-25', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Alex Green', '666.666.666-66', 5000.0, '1987-12-13', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Sidnei Lima', '777.777.777-77', 4200.0, '1989-01-28', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Marta Kent', '888.888.888-88', 4500.0, '1988-12-13', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Lois Lane', '999.999.999-99', 6000.0, '1993-12-07', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Felipe Augusto', '101.101.101-10', 3900.0, '1999-08-7', 2);
